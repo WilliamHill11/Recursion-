c8307b5a8caeb0b53788e77a98c9580e8f61880e
william hill <williamhill0831@Hotmail.com>
2023-08-07 22:57:03 -0400

    comment update

--- script.js
+++ script.js
@@ -80,6 +80,6 @@ const merge = (leftArr, rightArr) => {
   return result;
 };
 
-console.log(mergeSortRec([])); // []
+console.log(mergeSortRec([])); // calls empty list []
 console.log(mergeSortRec([10, 7, 8, 9, 1, 5])); // [1, 5, 7, 8, 9, 10]
 console.log(mergeSortRec([30, 20, 10, 50, 22, 33, 55])); //[10, 20, 22, 30, 33, 50, 55]
